9eccad67cd58b8b8cfb1a5bb4da45fb579616539
Zoe Ames <zoeames@gmail.com>
2015-10-24 13:20:23 -0500

    particles circles

--- public/js/main.js
+++ public/js/main.js
@@ -1,198 +1,114 @@
 if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
 
-var container;
+      var container;
 
-var camera, scene, renderer;
+      var camera, scene, renderer;
 
-var mesh;
+      var mesh;
 
-init();
-animate();
+      init();
+      animate();
 
-function init() {
+      function init() {
 
-  container = document.getElementById( 'container' );
+        container = document.getElementById( 'container' );
 
-  //
+        //
 
-  camera = new THREE.PerspectiveCamera( 27, window.innerWidth / window.innerHeight, 1, 3500 );
-  camera.position.z = 2750;
+        camera = new THREE.PerspectiveCamera( 27, window.innerWidth / window.innerHeight, 5, 3500 );
+        camera.position.z = 2750;
 
-  scene = new THREE.Scene();
-  scene.fog = new THREE.Fog( 0x050505, 2000, 3500 );
+        scene = new THREE.Scene();
+        scene.fog = new THREE.Fog( 0x050505, 2000, 3500 );
 
-  //
+        //
 
-  scene.add( new THREE.AmbientLight( 0x444444 ) );
+        var particles = 5000;
 
-  var light1 = new THREE.DirectionalLight( 0xffffff, 0.5 );
-  light1.position.set( 1, 1, 1 );
-  scene.add( light1 );
+        var geometry = new THREE.BufferGeometry();
 
-  var light2 = new THREE.DirectionalLight( 0xffffff, 1.5 );
-  light2.position.set( 0, -1, 0 );
-  scene.add( light2 );
+        var positions = new Float32Array( particles * 3 );
+        var colors = new Float32Array( particles * 3 );
 
-  //
+        var color = new THREE.Color();
 
-  var triangles = 160000;
+        var n = 1000, n2 = n / 2; // particles spread in the cube
 
-  var geometry = new THREE.BufferGeometry();
+        for ( var i = 0; i < positions.length; i += 3 ) {
 
-  var positions = new Float32Array( triangles * 3 * 3 );
-  var normals = new Float32Array( triangles * 3 * 3 );
-  var colors = new Float32Array( triangles * 3 * 3 );
+          // positions
 
-  var color = new THREE.Color();
+          var x = Math.random() * n - n2;
+          var y = Math.random() * n - n2;
+          var z = Math.random() * n - n2;
 
-  var n = 800, n2 = n/2;  // triangles spread in the cube
-  var d = 12, d2 = d/2; // individual triangle size
+          positions[ i ]     = x;
+          positions[ i + 1 ] = y;
+          positions[ i + 2 ] = z;
 
-  var pA = new THREE.Vector3();
-  var pB = new THREE.Vector3();
-  var pC = new THREE.Vector3();
+          // colors
 
-  var cb = new THREE.Vector3();
-  var ab = new THREE.Vector3();
+          var vx = ( x / n ) + 0.5;
+          var vy = ( y / n ) + 0.5;
+          var vz = ( z / n ) + 0.5;
 
-  for ( var i = 0; i < positions.length; i += 9 ) {
+          color.setRGB( vx, vy, vz );
 
-    // positions
+          colors[ i ]     = color.r;
+          colors[ i + 1 ] = color.g;
+          colors[ i + 2 ] = color.b;
 
-    var x = Math.random() * n - n2;
-    var y = Math.random() * n - n2;
-    var z = Math.random() * n - n2;
+        }
 
-    var ax = x + Math.random() * d - d2;
-    var ay = y + Math.random() * d - d2;
-    var az = z + Math.random() * d - d2;
+        geometry.addAttribute( 'position', new THREE.BufferAttribute( positions, 3 ) );
+        geometry.addAttribute( 'color', new THREE.BufferAttribute( colors, 3 ) );
 
-    var bx = x + Math.random() * d - d2;
-    var by = y + Math.random() * d - d2;
-    var bz = z + Math.random() * d - d2;
+        geometry.computeBoundingSphere();
 
-    var cx = x + Math.random() * d - d2;
-    var cy = y + Math.random() * d - d2;
-    var cz = z + Math.random() * d - d2;
+        //
 
-    positions[ i ]     = ax;
-    positions[ i + 1 ] = ay;
-    positions[ i + 2 ] = az;
+        var material = new THREE.PointsMaterial( { size: 15, vertexColors: THREE.VertexColors } );
 
-    positions[ i + 3 ] = bx;
-    positions[ i + 4 ] = by;
-    positions[ i + 5 ] = bz;
+        particleSystem = new THREE.Points( geometry, material );
+        scene.add( particleSystem );
 
-    positions[ i + 6 ] = cx;
-    positions[ i + 7 ] = cy;
-    positions[ i + 8 ] = cz;
+        //
+        renderer = new THREE.WebGLRenderer( { antialias: false } );
+        renderer.setClearColor( scene.fog.color );
+        renderer.setPixelRatio( window.devicePixelRatio );
+        renderer.setSize( window.innerWidth, window.innerHeight );
 
-    // flat face normals
+        container.appendChild( renderer.domElement );
 
-    pA.set( ax, ay, az );
-    pB.set( bx, by, bz );
-    pC.set( cx, cy, cz );
+        window.addEventListener( 'resize', onWindowResize, false );
 
-    cb.subVectors( pC, pB );
-    ab.subVectors( pA, pB );
-    cb.cross( ab );
+      }
 
-    cb.normalize();
+      function onWindowResize() {
 
-    var nx = cb.x;
-    var ny = cb.y;
-    var nz = cb.z;
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
 
-    normals[ i ]     = nx;
-    normals[ i + 1 ] = ny;
-    normals[ i + 2 ] = nz;
+        renderer.setSize( window.innerWidth, window.innerHeight );
 
-    normals[ i + 3 ] = nx;
-    normals[ i + 4 ] = ny;
-    normals[ i + 5 ] = nz;
+      }
 
-    normals[ i + 6 ] = nx;
-    normals[ i + 7 ] = ny;
-    normals[ i + 8 ] = nz;
+      //
 
-    // colors
+      function animate() {
 
-    var vx = ( x / n ) + 0.5;
-    var vy = ( y / n ) + 0.5;
-    var vz = ( z / n ) + 0.5;
+        requestAnimationFrame( animate );
 
-    color.setRGB( vx, vy, vz );
+        render();
 
-    colors[ i ]     = color.r;
-    colors[ i + 1 ] = color.g;
-    colors[ i + 2 ] = color.b;
+      }
 
-    colors[ i + 3 ] = color.r;
-    colors[ i + 4 ] = color.g;
-    colors[ i + 5 ] = color.b;
+      function render() {
 
-    colors[ i + 6 ] = color.r;
-    colors[ i + 7 ] = color.g;
-    colors[ i + 8 ] = color.b;
+        var time = Date.now() * 0.001;
 
-  }
+        particleSystem.rotation.y = time * 0.25;
 
-  geometry.addAttribute( 'position', new THREE.BufferAttribute( positions, 3 ) );
-  geometry.addAttribute( 'normal', new THREE.BufferAttribute( normals, 3 ) );
-  geometry.addAttribute( 'color', new THREE.BufferAttribute( colors, 3 ) );
+        renderer.render( scene, camera );
 
-  geometry.computeBoundingSphere();
-
-  var material = new THREE.MeshPhongMaterial( {
-    color: 0xaaaaaa, specular: 0xffffff, shininess: 250,
-    side: THREE.DoubleSide, vertexColors: THREE.VertexColors
-  } );
-
-  mesh = new THREE.Mesh( geometry, material );
-  scene.add( mesh );
-
-  //
-
-  renderer = new THREE.WebGLRenderer( { antialias: false } );
-  renderer.setClearColor( scene.fog.color );
-  renderer.setPixelRatio( window.devicePixelRatio );
-  renderer.setSize( window.innerWidth, window.innerHeight );
-
-  renderer.gammaInput = true;
-  renderer.gammaOutput = true;
-
-  container.appendChild( renderer.domElement );
-
-  window.addEventListener( 'resize', onWindowResize, false );
-
-}
-
-function onWindowResize() {
-
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-
-  renderer.setSize( window.innerWidth, window.innerHeight );
-
-}
-
-//
-
-function animate() {
-
-  requestAnimationFrame( animate );
-
-  render();
-
-}
-
-function render() {
-
-  var time = Date.now() * 0.001;
-
-  mesh.rotation.x = time * 0.25;
-  mesh.rotation.y = time * 0.5;
-
-  renderer.render( scene, camera );
-
-}
+      }
